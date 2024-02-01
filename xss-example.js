556cc5a925dc865bb2fb4436887bcf5991319b4c
haggai yaniv <yhaggai@gmail.com>
2024-02-01 11:59:31 +0200

    Update xss-example.js

--- xss-example.js
+++ xss-example.js
@@ -7,3 +7,5 @@ var unsafe_div = window.document.getElementById("vulnerable-div");
 unsafe_div.innerHTML = username+  "Hello to you 1";
 unsafe_div.innerHTML = username+  "Hello to you 2";
 
+console.log('hello')
+
